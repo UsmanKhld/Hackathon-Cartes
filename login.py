f2b284e64e5da4a8673ca5dd1be1c83f2bf8224c
Izaan Khalid <izaan.khalid@outlook.com>
2024-03-23 23:30:12 -0500

    commit

---
+++ login.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+import psycopg2.extras
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError, Email, EqualTo
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+@app.route('/restaurant-login', methods=['POST'])
+def submitform():
+    email=request.form.get('email')
+    password=request.form.get('password')
+    print("Received email:", email)
+    print("Received password:", password)
+    return 'Form Submitted!'
+
+if __name__ == '__main__': 
+   app.run(debug = True) 

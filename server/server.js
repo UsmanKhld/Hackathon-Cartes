172737e38826cd305ae50eaff453ed3d5acc61cd
Izaan Khalid <izaan.khalid@outlook.com>
2024-03-24 05:16:05 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/users', async (req, res) => {
-    const user_email = 'McDonalds@gmail.com';
+app.get('/users/:user_email', async (req, res) => {
+    console.log(req);
+    const user_email = req.params;
     try {
         const users = await pool.query('SELECT * FROM supplier WHERE email = $1', [user_email]);
         res.json(users.rows);
